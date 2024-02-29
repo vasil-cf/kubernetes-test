2fd0f8f59ecfa3cb39c357a4e92a226978926613
Vasil Sudakou <vasil.sudakov@codefresh.io>
2024-02-29 09:46:20 +0300

    feat: app1 works

---
+++ app1/src/main.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify';
+
+const server = fastify({logger: true});
+
+server.get('/', async (request, reply) => {
+    return {hello: 'world'};
+});
+
+const start = async () => {
+    try {
+        const port = process.env.port!;
+        const host = process.env.host!;
+        await server.listen({port: Number(port), host});
+        console.log(`Server is listening on ${port}`);
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
